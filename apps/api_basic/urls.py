036e85b58c429e81a16c333d385284aeb8a36701
test <test@test.test>
2020-08-09 21:25:42 +0300

    Added suffix

--- apps/api_basic/urls.py
+++ apps/api_basic/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ArticleList, ArticleDetail
 
 app_name = 'apps.api_basic'
@@ -7,3 +8,6 @@ urlpatterns = [
     path('article/', ArticleList.as_view()),
     path('article/<int:pk>', ArticleDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
