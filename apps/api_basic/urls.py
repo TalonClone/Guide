557655d02a4448d186a30d08e434d8ccd0d6c7af
test <test@test.test>
2020-08-09 13:40:53 +0300

    Change to class based view

--- apps/api_basic/urls.py
+++ apps/api_basic/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView
 
 app_name = 'apps.api_basic'
 
 urlpatterns = [
-    path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
     path('detail/<int:pk>', article_detail),
 ]
--- apps/api_basic/views.py
+++ apps/api_basic/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from .models import Article
 from .serializers import ArticleSerializer
 
 
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == 'GET':
+class ArticleAPIView(APIView):
+
+    def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request):
         serializer = ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
