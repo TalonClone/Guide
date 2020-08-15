d20e14ff22a39a7d0574e3460481939d6738a24d
test <test@test.test>
2020-08-15 19:09:57 +0300

    Settings static fix

--- Guide/settings.py
+++ Guide/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Guide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Media Files
 MEDIA_URL = '/media/'
