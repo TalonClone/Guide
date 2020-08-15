bd59987ece5ca79549502457b845f0dcd0ddab07
test <test@test.test>
2020-08-15 19:10:32 +0300

    create view and url for movies app

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from .views import MoviesView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 app_name = 'apps.movies'
 
 urlpatterns = [
-    #path('movies/', ArticleList.as_view()),
+    path('movies/', MoviesView.as_view()),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
